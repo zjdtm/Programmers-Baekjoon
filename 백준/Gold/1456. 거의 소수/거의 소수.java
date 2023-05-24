ad351b9584427e7cb7ed5a57c54f55667529d84a
YongHyun <syh05247@naver.com>
2023-05-24 15:52:07 +0900

    [Gold V] Title: 거의 소수, Time: 484 ms, Memory: 94176 KB -BaekjoonHub

---
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long Min = Long.parseLong(st.nextToken());
+        long Max = Long.parseLong(st.nextToken());
+        long[] A = new long[10000001];
+        for(int i = 2; i < A.length; i++) {
+            A[i] = i;
+        }
+
+        for(int i = 2; i <= Math.sqrt(A.length); i++) {
+            if(A[i] == 0)
+                continue;
+
+            for(int j = i + i; j < A.length; j += i){
+                A[j] = 0;
+            }
+        }
+
+        int count = 0;
+        for(int i = 2; i < 10000001; i++) {
+            if(A[i] != 0) {
+                long temp = A[i];
+                while ((double)A[i] <= (double) Max / (double) temp) {
+                    if ((double)A[i] >= (double) Min / (double) temp) {
+                        count++;
+                    }
+                    temp = temp * A[i];
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
