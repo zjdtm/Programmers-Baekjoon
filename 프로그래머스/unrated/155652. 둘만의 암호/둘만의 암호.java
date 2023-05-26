c203af12e9cdc3984d3a6642aa11dbe03c22f3c8
YongHyun <syh05247@naver.com>
2023-05-26 16:13:03 +0900

    [unrated] Title: 둘만의 암호, Time: 1.07 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -3,36 +3,20 @@ import java.io.*;
 
 class Solution {
     public String solution(String s, String skip, int index) {
-        StringBuilder sb = new StringBuilder();
-        Queue<Character> queue = new LinkedList();
+        StringBuilder answer = new StringBuilder();
 
-        for (int i = 0; i < 26; i++) {
-            queue.add((char) (i + 97));
-        }
-
-        for (int i = 0; i < s.length(); i++) {
-            char alphabat = s.charAt(i);
-            int count = index;
-
-            while (true) {
-                if (queue.peek() == alphabat) {
-                    char c = ' ';
-                    queue.add(queue.poll());
-                    while (count != 0) {
-                        c = queue.poll();
-                        if (!skip.contains(String.valueOf(c))) {
-                            count--;
-                        }
-                        queue.add(c);
-                    }
-                    sb.append(c);
-                    break;
-                } else {
-                    char poll = queue.poll();
-                    queue.add(poll);
+        for (char letter : s.toCharArray()) {
+            char temp = letter;
+            int idx = 0;
+            while (idx < index) {
+                temp = temp == 'z' ? 'a' : (char) (temp + 1);
+                if (!skip.contains(String.valueOf(temp))) {
+                    idx += 1;
                 }
             }
+            answer.append(temp);
         }
-        return sb.toString();
+
+        return answer.toString();
     }
 }
