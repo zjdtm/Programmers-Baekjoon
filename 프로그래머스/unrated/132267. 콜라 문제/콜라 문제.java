86e3e4b94d9f32d80a0244e8c9119b632b54ae2b
YongHyun <syh05247@naver.com>
2023-06-08 13:26:02 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(true) {
+            if(n / a == 0)
+                break;
+            answer += (n / a) * b;
+            n = (n / a) * b + n % a;
+        }
+        return answer;
+    }
+}
