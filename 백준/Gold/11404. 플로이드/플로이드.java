e9fe703834b3390aa4502f854b38f3bf2f5def94
YongHyun <syh05247@naver.com>
2023-06-21 11:39:40 +0900

    [Gold IV] Title: 플로이드, Time: 672 ms, Memory: 45268 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+    static int distance[][];
+
+    public static void main(String[] args) throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        distance = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i == j)
+                    distance[i][j] = 0;
+                else
+                    distance[i][j] = 10000001;
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            if (distance[s][e] > v) distance[s][e] = v;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (distance[i][j] > distance[i][k] + distance[k][j])
+                        distance[i][j] = distance[i][k] + distance[k][j];
+                }
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (distance[i][j] == 10000001) System.out.print("0 ");
+                else System.out.print(distance[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
