bde55dcb1146c0c326dcf8ff2dbf7064bd9c63db
YongHyun <syh05247@naver.com>
2023-07-25 00:58:40 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,21 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         StringBuilder sb = new StringBuilder();
 
         while(n != 0) {
             sb.append(n % 3);
             n /= 3;
         }
+        
+        return Integer.parseInt(sb.toString(), 3);
 
-        int len = 0;
-        for(int i = sb.length() - 1; i >= 0; i--) {
-            answer += Integer.parseInt(sb.substring(i, i + 1)) * Math.pow(3, len);
-            len += 1;
-        }
-
-        return answer;
     }
 }
