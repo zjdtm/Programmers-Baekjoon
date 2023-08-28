ebeb57209ef57b0247e0497c2165bfa6050aaff8
YongHyun <syh05247@naver.com>
2023-08-28 14:25:57 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -2,22 +2,24 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
 
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
+        for(int i = 0; i < nums.length - 2; i++) {
+            for(int j = i + 1; j < nums.length - 1; j++) {
                 for(int k = j + 1; k < nums.length; k++) {
-                    int checkNumber = nums[i] + nums[j] + nums[k];
-                    boolean isPrime = true;
-                    for(int l = 2; l < checkNumber; l++) {
-                        if(checkNumber % l == 0) {
-                            isPrime = false;
-                        }
-                    }
-                    if(isPrime)
+                    if(primeCheck(nums[i] + nums[j] + nums[k])){
                         answer++;
+                    }
                 }
             }
         }
-
         return answer;
+    }
+
+
+    static boolean primeCheck(int num) {
+        int cnt = 0;
+        for(int i = 1; i <= (int) Math.sqrt(num); i ++){
+            if(num % i == 0) cnt += 1;
+        }
+        return cnt == 1;
     }
 }
