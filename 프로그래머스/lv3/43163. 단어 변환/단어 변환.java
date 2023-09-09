9f65d6a75c7f43eef9369e731fad3c4e20e5bce6
YongHyun <syh05247@naver.com>
2023-09-09 16:26:02 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,39 @@
+class Solution {
+    static boolean[] visited;
+    static int answer = 0;
+
+    public int solution(String begin, String target, String[] words) {
+
+        visited = new boolean[words.length];
+
+        DFS(begin, target, words, 0);
+
+        return answer;
+    }
+
+    static void DFS(String begin, String target, String[] words, int cnt) {
+        if (begin.equals(target)) {
+            answer = cnt;
+            return;
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            int k = 0;
+            for (int j = 0; j < begin.length(); j++) {
+                if (begin.charAt(j) != words[i].charAt(j)) {
+                    k++;
+                }
+            }
+
+            if (k == 1) {
+                visited[i] = true;
+                DFS(words[i], target, words, cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
