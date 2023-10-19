3ce3cd508acc5862fa074a2fb293611e33ce0f44
YongHyun <syh05247@naver.com>
2023-10-19 21:11:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -3,7 +3,7 @@ class Solution {
         String[] alphabat = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
             for (int i = 0; i < alphabat.length; i++) {
-                s = s.replaceAll(alphabat[i], String.valueOf(i));
+                s = s.replace(alphabat[i], Integer.toString(i));
             }
 
             return Integer.parseInt(s);
