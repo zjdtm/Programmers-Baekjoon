16737c308c6f9449d20c5edf7038b16f8ddd3a2f
YongHyun <syh05247@naver.com>
2023-11-14 13:20:03 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.30 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,28 +1,51 @@
 import java.util.*;
 
 class Solution {
-    private static class Point {
-        public final long x, y;
+        public String[] solution(int[][] line) {
 
-        private Point(long x, long y) {
-            this.x = x;
-            this.y = y;
+        List<Point> points = new ArrayList<>();
+
+        // 모든 직성 쌍에 대해 반복
+        for (int i = 0; i < line.length; i++) {
+            for (int j = i + 1; j < line.length; j++) {
+                Point intersection = intersection(line[i][0], line[i][1], line[i][2],
+                        line[j][0], line[j][1], line[j][2]);
+
+                // 정수 좌표만 저장
+                if (intersection != null) {
+                    points.add(intersection);
+                }
+            }
         }
-    }
 
-    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
+        // 구한 최댓값, 최솟값을 이용하여 2차원 배열의 크기 결정
+        Point minimum = getMinimumPoint(points);
+        Point maximum = getMaximumPoint(points);
 
-        double x = (double) (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
-        double y = (double) (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
+        int width = (int) (maximum.x - minimum.x + 1);
+        int height = (int) (maximum.y - minimum.y + 1);
+        char[][] arr = new char[height][width];
+        for (char[] row : arr) {
+            Arrays.fill(row, '.');
+        }
 
+        for (Point p : points) {
+            // 2차원 배열에 빌 찍기
+            int x = (int) (p.x - minimum.x);
+            int y = (int) (maximum.y - p.y);
+            arr[y][x] = '*';
+        }
 
-        if (x % 1 != 0 || y % 1 != 0) return null;
+        String[] result = new String[arr.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new String(arr[i]);
+        }
 
-        return new Point((long) x, (long) y);
-
+        return result;
     }
 
     private Point getMinimumPoint(List<Point> points) {
+        // 가장 작은 좌표 찾기
         long x = Long.MAX_VALUE;
         long y = Long.MAX_VALUE;
 
@@ -35,6 +58,7 @@ class Solution {
     }
 
     private Point getMaximumPoint(List<Point> points) {
+        // 가장 큰 좌표 찾기
         long x = Long.MIN_VALUE;
         long y = Long.MIN_VALUE;
 
@@ -46,40 +70,22 @@ class Solution {
         return new Point(x, y);
     }
 
-    public String[] solution(int[][] line) {
-        List<Point> points = new ArrayList<>();
+    // 교점 좌표 구하기
+    private Point intersection(long a1, long b1, long c1,
+                               long a2, long b2, long c2) {
+        double x = (double) (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
+        double y = (double) (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
 
-        for (int i = 0; i < line.length; i++) {
-            for (int j = i + 1; j < line.length; j++) {
-                Point intersection = intersection(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
+        if (x % 1 != 0 || y % 1 != 0) return null;
 
-                if (intersection != null) {
-                    points.add(intersection);
-                }
-            }
+        return new Point((long) x, (long) y);
+    }
+
+    private static class Point {
+        public final long x, y;
+        private Point(long x, long y) {
+            this.x = x;
+            this.y = y;
         }
-
-        Point minimum = getMinimumPoint(points);
-        Point maximum = getMaximumPoint(points);
-
-        int width = (int) (maximum.x - minimum.x + 1);
-        int height = (int) (maximum.y - minimum.y + 1);
-
-        char[][] arr = new char[height][width];
-        for (char[] row : arr) {
-            Arrays.fill(row, '.');
-        }
-
-        for (Point p : points) {
-            int x = (int) (p.x - minimum.x);
-            int y = (int) (maximum.y - p.y);
-            arr[y][x] = '*';
-        }
-
-        String[] result = new String[arr.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = new String(arr[i]);
-        }
-        return result;
     }
 }
