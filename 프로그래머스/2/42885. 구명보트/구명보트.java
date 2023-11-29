89f3fdda6eeb350dbaedce1f658bec2db99648af
YongHyun <syh05247@naver.com>
2023-11-29 14:52:39 +0900

    [level 2] Title: 구명보트, Time: 12.34 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int left = 0;
+        int right = people.length - 1;
+        Arrays.sort(people);
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+                right--;
+            } else {
+                right--;
+            }
+            answer++;
+        }
+
+        return answer;
+    }
+}
