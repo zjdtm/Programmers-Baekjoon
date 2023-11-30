c8da1c3bea567136850cb3ed86cea9fb6f8f6cbf
YongHyun <syh05247@naver.com>
2023-11-30 13:12:37 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,10 +1,10 @@
 class Solution {
     public long solution(int n) {
-        int[] dp = new int[2001];
+        long[] dp = new long[2001];
         dp[1] = 1;
         dp[2] = 2;
 
-        for (int i = 3; i < 2001; i++) {
+        for (int i = 3; i <= n; i++) {
             dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
         }
 
