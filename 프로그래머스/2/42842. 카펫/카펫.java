96375aa75b291d79c0e3882f0f9fa6ebd63a3e50
YongHyun <syh05247@naver.com>
2024-06-02 10:22:40 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,15 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-            for (int width = 3; width <= 5000; width++) {
-                for (int height = 3; height <= width; height++) {
-                    int boundary = (width + height - 2) * 2;
-                    int center = width * height - boundary;
-                    
-                    if (brown == boundary && yellow == center) {
-                        return new int[] {width, height};
-                    }
-                }
+        for (int width = 3; width <= 5000; width++) {
+            for (int height = 3; height <= width; height++) {
+                int boundary = (width + height - 2) * 2;
+                int center = width * height - boundary;
+
+                if (brown == boundary && yellow == center)
+                    return new int[]{ width, height };
             }
-            return null;
         }
+
+        return null;
+    }
 }
