2b13e8b650b4c875ca4f60f36c9b7b38b1601c28
YongHyun <syh05247@naver.com>
2024-06-12 21:24:07 +0900

    [level 2] Title: 가장 큰 수, Time: 4.92 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -5,9 +5,10 @@ class Solution {
     public String solution(int[] numbers) {
         return Arrays.stream(numbers)
                 .mapToObj(String::valueOf)
-                .sorted((s1, s2) -> {
-                    int original = Integer.parseInt(s1 + s2);
-                    int reversed = Integer.parseInt(s2 + s1);
+                .sorted((v1, v2) -> {
+                    int original = Integer.parseInt(v1 + v2);
+                    int reversed = Integer.parseInt(v2 + v1);
+
                     return reversed - original;
                 })
                 .collect(Collectors.joining(""))
