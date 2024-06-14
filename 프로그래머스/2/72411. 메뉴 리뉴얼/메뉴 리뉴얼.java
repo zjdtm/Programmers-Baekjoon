bdc3b97f45cefa33200a0e449aa3b4ecd16819c1
YongHyun <syh05247@naver.com>
2024-06-14 21:40:40 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 22.32 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,70 +1,54 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
-    private static class Course {
-        public final String course;
-        public final int occurrences;
+    List<String> answerList = new ArrayList<>();
+    Map<String, Integer> hashMap = new HashMap<>();
 
-        public Course(String course, int occurrences) {
-            this.course = course;
-            this.occurrences = occurrences;
-        }
-    }
-
-    private void getCourses(char nextMenu, Set<String> selectedMenus,
-                            List<Set<String>> orderList, Map<Integer, List<Course>> courses) {
-        int occurrences = (int) orderList.stream()
-                .filter(order -> order.containsAll(selectedMenus))
-                .count();
-        if (occurrences < 2) return;
-
-        int size = selectedMenus.size();
-        if (courses.containsKey(size)) {
-            List<Course> courseList = courses.get(size);
-            Course course = new Course(selectedMenus.stream()
-                    .sorted()
-                    .collect(Collectors.joining("")), occurrences);
-            Course original = courseList.get(0);
-            if (original.occurrences < occurrences) {
-                courseList.clear();
-                courseList.add(course);
-            } else if (original.occurrences == occurrences) {
-                courseList.add(course);
-            }
+    public void combination(String order, String others, int count) {
+        if (count == 0) {
+            hashMap.put(order, hashMap.getOrDefault(order, 0) + 1);
+            return;
         }
 
-        if (size >= 10) return;
-        for (char menuChar = nextMenu; menuChar <= 'Z'; menuChar++) {
-            String menu = String.valueOf(menuChar);
-            selectedMenus.add(menu);
-            getCourses((char) (menuChar + 1), selectedMenus, orderList, courses);
-            selectedMenus.remove(menu);
+        for (int i = 0; i < others.length(); i++) {
+            combination(order + others.charAt(i),
+                    others.substring(i + 1),
+                    count - 1);
         }
     }
 
     public String[] solution(String[] orders, int[] course) {
-        List<Set<String>> orderList = Arrays.stream(orders)
-                .map(String::chars)
-                .map(charStream -> charStream
-                        .mapToObj(menu -> String.valueOf((char) menu))
-                        .collect(Collectors.toSet()))
-                .collect(Collectors.toList());
-
-        Map<Integer, List<Course>> courses = new HashMap<>();
-        for (int length : course) {
-            List<Course> list = new ArrayList<>();
-            list.add(new Course("", 0));
-            courses.put(length, list);
+        for (int i = 0; i < orders.length; i++) {
+            char[] arr = orders[i].toCharArray();
+            Arrays.sort(arr);
+            orders[i] = String.valueOf(arr);
         }
 
-        getCourses('A', new HashSet<>(), orderList, courses);
+        for (int courseLength : course) {
+            for (String order : orders)
+                combination("", order, courseLength);
 
-        return courses.values().stream()
-                .filter(list -> list.get(0).occurrences > 0)
-                .flatMap(List::stream)
-                .map(c -> c.course)
-                .sorted()
-                .toArray(String[]::new);
+
+            if (!hashMap.isEmpty()) {
+                List<Integer> countList = new ArrayList<>(hashMap.values());
+                int max = Collections.max(countList);
+
+                if (max > 1)
+                    for (String key : hashMap.keySet())
+                        if (hashMap.get(key) == max)
+                            answerList.add(key);
+
+                hashMap.clear();
+
+            }
+        }
+
+        Collections.sort(answerList);
+        String[] answer = new String[answerList.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answerList.get(i);
+        }
+        return answer;
     }
 }
